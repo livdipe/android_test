b355160f14a3be51bf90269c1b2f189890c40175
lichunli <livdipe@gmail.com>
2015-05-25 00:52:28 +0800

    init android project

---
+++ test/src/com/abc/abc/UnityPlayerActivity.java
@@ -0,0 +1,8 @@
+package com.abc.abc;
+
+import com.unity3d.player.*;
+
+/**
+ * @deprecated Use UnityPlayerNativeActivity instead.
+ */
+public class UnityPlayerActivity extends UnityPlayerNativeActivity { }
